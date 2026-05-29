99400ec69050dfbba5e05ff38f568ad20403d988
JGlassMSI <jeff.glass@msichicago.org>
2026-05-29 08:26:54 -0700

    Adjust lighting script

--- send_lighting_data.bat
+++ send_lighting_data.bat
@@ -1,3 +1,3 @@
-echo Program will start in 30 seconds
-timeout /t 30
+echo Program will start in 15 seconds
+timeout /t 15
 uv run python C:\Users\MSI\Blue-Paradox\scripts\send.py --debug --color blue -u 47-160
