e0c8a2bcd0b3b2957cff8641092f322ad38551f8
JGlassMSI <jeff.glass@msichicago.org>
2026-05-29 08:26:53 -0700

    Add more command line args to sender

--- scripts/send.py
+++ scripts/send.py
@@ -9,7 +9,8 @@ from _utils import drive_device
 
 parser = argparse.ArgumentParser("Send sacn lighting data")
 
-parser.add_argument("--debug", action="store_true")
+parser.add_argument("--debug", action="store_true", help="Send a fixed blue value to all universes")
+parser.add_argument("--color", choices=["red", "green", "blue"], help="debug color (only valid with --debug)")
 parser.add_argument("-u", "--universes", type=str, help="a comma- or dash-separated list of universes to capture", default="47-69")
 parser.add_argument("-f", "--file", help="json data file to open", default=r"C:\Users\trex\Documents\Blue Paradox\dmx_data_u47-69_t20260529_084747.json")
 
@@ -17,12 +18,23 @@ parser.add_argument("-f", "--file", help="json data file to open", default=r"C:\
 _cid = (0x3b, 0xc6, 0x7d, 0x92, 0x99, 0x5f, 0x19, 0xdc, 0x6, 0x67, 0x7e, 0x5e, 0xf4, 0x95, 0xd5, 0x2c)
 sender = sacn.sACNsender(source_name="BP-Room-4-NUC", cid=_cid)
 
+DEBUG_LEVEL = 200
+DEBUG_DATA = {
+    'red'  : [DEBUG_LEVEL, 0, 0],
+    'green': [0, DEBUG_LEVEL, 0],
+    'blue' : [0, 50, DEBUG_LEVEL]
+}
+
 
 if __name__ == "__main__":
     args = parser.parse_args()
+    if args.color and not args.debug:
+        raise argparse.ArgumentError("--color is only valid with --debug")
+    if args.debug and not args.color:
+        args.color = "blue"
     universes = parse_ranges_str(args.universes)
     if args.debug:
-        data = {str(u): [20] * 512 for u in universes}
+        data = {str(u): [*DEBUG_DATA[args.color] * (int(512/3)+1)][:512] for u in universes}
     else:
         DATA_FILE = args.file
 
@@ -42,7 +54,7 @@ if __name__ == "__main__":
         sender.activate_output(universe)
         sender[universe].multicast = True  # set multicast to True
         sender[universe].dmx_data = data[str(universe)]
-        sender[universe].priority = 200
+        sender[universe].priority = 190
 
     print(f"Sending dmx data on universes {args.universes}")
     print(f"Press ctrl+c to exit")
